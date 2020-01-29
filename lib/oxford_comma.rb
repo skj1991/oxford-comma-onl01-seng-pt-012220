41ed15c01917d634afe164858e3559274fd46803
Stacy Johns <stacykjohns@icloud.com>
2020-01-29 18:31:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.last.insert(0, "and ")
+    array.join(", ")
+  end
 end
